36a9d85999723c319b9ac22030b0708e76d443d9
Hsun <58354107+Hsun1031@users.noreply.github.com>
2021-11-13 15:46:22 +0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # ESP32GithubOTA
+
 ESP32 over the air programming by ESP32
+
+## 
+* function
+  * [Update Spiffs Data](https://github.com/Hsun1031/ESP32GithubOTA#Update-Spiffs-Data)
+  * [Update ESP32 Firmware](https://github.com/Hsun1031/ESP32GithubOTA#Update-ESP32-Firmware)
+  * [Get the Error Code](https://github.com/Hsun1031/ESP32GithubOTA#Get-the-Error-Code)
+## function
+
+GithubUrl: `https://raw.githubusercontent.com/<Your name>/<Your repo>/<Your branch>/<Your file path>/<Your file name>`.<br>
+example: `https://raw.githubusercontent.com/Hsun1031/ESP32GithubOTA/master/example/bin/firmware.bin`<br>
+Github Token: `ghp_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`
+
+### Update Spiffs Data
+
+```c++
+/**
+ * @brief Update spiffs data.
+ * 
+ * @param spiffsUrl Github url https://raw.githubusercontent.com/<Your name>/<Your repo>/<Your branch>/<Your file path>/<Your file name>.
+ * @param token Github Token.
+ * @return true  Update Susses.
+ * @return false Update or Http client faild. 
+ */
+bool spiffsOTA  (const char *spiffsUrl, const char *token);
+```
+
+### Update ESP32 Firmware
+
+```c++
+/**
+ * @brief Update ESP32 firmware.
+ * 
+ * @param firmwareUrl Github url https://raw.githubusercontent.com/<Your name>/<Your repo>/<Your branch>/<Your file path>/<Your file name>.
+ * @param token Github Token.
+ * @return true  Update Susses.
+ * @return false Update or Http client faild.
+ */
+bool firmwareOTA(const char *firmwareUrl, const char *token);
+```
+
+### Get the Error Code
+
+```C++
+/**
+* @brief Get the Error Code 
+* 
+* @return 0 -> OK
+* @return 1 -> update begin error
+* @return 2 -> update writeStream error
+* @return 3 -> update end error.
+* @return 404 -> Url error or token
+*/
+uint8_t getErrorCode(){ return errorCode; };
+```
--- src/ESP32GithubOTA.h
+++ src/ESP32GithubOTA.h
@@ -1,3 +1,13 @@
+/**
+ * @file ESP32GithubOTA.h
+ * @author Hsun (hsun1031@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef ESP32_Github_OTA
 #define ESP32_Github_OTA
 
@@ -9,14 +19,61 @@
     class ESP32GithubOTA {
     private:
         uint8_t errorCode;
+
+        /**
+         * @brief 
+         * 
+         * @param data WiFiClient *
+         * @param size http.getSize()
+         * @param command U_FLASH or U_SPIFFS.
+         * @return true  Update Susses.
+         * @return false Update or Http client faild.
+         */
         bool runUpdate(Stream& data, size_t size, int command);
+
+        /**
+         * @brief 
+         * 
+         * @param url Github url https://raw.githubusercontent.com/<Your name>/<Your repo>/<Your branch>/<Your file path>/<Your file name>.
+         * @param token Github Token.
+         * @param command U_FLASH or U_SPIFFS.
+         * @return true 
+         * @return false 
+         */
         bool clientGithub(const char *url, const char *token, int command);
        
     public:
         ESP32GithubOTA ();
 
+        /**
+         * @brief Update spiffs data.
+         * 
+         * @param spiffsUrl Github url https://raw.githubusercontent.com/<Your name>/<Your repo>/<Your branch>/<Your file path>/<Your file name>.
+         * @param token Github Token.
+         * @return true  Update Susses.
+         * @return false Update or Http client faild. 
+         */
         bool spiffsOTA  (const char *spiffsUrl, const char *token);
+
+        /**
+         * @brief Update ESP32 firmware.
+         * 
+         * @param firmwareUrl Github url https://raw.githubusercontent.com/<Your name>/<Your repo>/<Your branch>/<Your file path>/<Your file name>.
+         * @param token Github Token.
+         * @return true  Update Susses.
+         * @return false Update or Http client faild.
+         */
         bool firmwareOTA(const char *firmwareUrl, const char *token);
+
+        /**
+         * @brief Get the Error Code 
+         * 
+         * @return 0 -> OK
+         * @return 1 -> update begin error
+         * @return 2 -> update writeStream error
+         * @return 3 -> update end error.
+         * @return 404 -> Url error or token
+         */
         uint8_t getErrorCode(){ return errorCode; };
     };
 
